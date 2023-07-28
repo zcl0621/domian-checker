f7a7ba1d97eeb6211ef1ca089b887579e26bb273
zcl0621 <zcl0621@hotmail.com>
2023-07-28 10:30:22 +0800

    修改版本号

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build --platform linux/amd64 -t dns-backend:42 . && \
-docker save -o backend-42.tar dns-backend:42 && \
-scp backend-42.tar dns_vps:/tmp/backend-42.tar && \
-rm -rf backend-42.tar && \
-docker rmi dns-backend:42
+docker build --platform linux/amd64 -t dns-backend:43 . && \
+docker save -o backend-43.tar dns-backend:43 && \
+scp backend-43.tar dns_vps:/tmp/backend-43.tar && \
+rm -rf backend-43.tar && \
+docker rmi dns-backend:43
